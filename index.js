83d76e493d9fa03a57ce8125cc936f7b91707e75
Charles Feval <chfeval@microsoft.com>
2020-02-17 16:10:53 -0800

    Todo management

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const { parseCommandLine } = require("yaclip");
 const notes = require("./src/notes");
-
+const todo = require("./src/todo");
 
 const options = [
-  { name: "notes", alias: "n", type: Boolean, multiple: false, subcommands: notes.options }
+  { name: "notes", alias: "n", type: Boolean, multiple: false, subcommands: notes.options },
+  { name: "todo", alias: "t", type: Boolean, multiple: false, subcommands: todo.options },
 ]
 
 const command = parseCommandLine(options, { dashesAreOptional: true });
 
 try {
-
   if (command.notes) {
     notes.command(command.notes);
+  } else if (command.todo) {
+    todo.command(command.todo);
   }
 }
 catch (error) {
---
+++ src/todo.js
@@ -0,0 +1,59 @@
+const fs = require("fs");
+const path = require("path");
+
+const COMMAND_LIST_TODO = "list";
+const SUBCOMMAND_PATH = "path"
+
+const subcommands = [
+  { name: SUBCOMMAND_PATH, alias: "p", type: String }
+]
+
+const todoOptions = [
+  { name: COMMAND_LIST_TODO, alias: "l", type: Boolean, subcommands },
+]
+
+const todoRegex = RegExp("{t(?:odo)?(?::|\s)*([^}]+)}", "gi");
+
+const findTodoInFile = (file) => {
+  const content = fs.readFileSync(file).toString();
+  const regexResult = content.match(todoRegex);
+  if (regexResult) {
+
+    return regexResult.map(text => ({ file, text: text.replace(todoRegex, "$1") }));
+  } else {
+    return []
+  }
+}
+
+const findTodoInPath = (p) => {
+  const files = fs.readdirSync(p);
+  return files.map((file) => {
+    const filePath = path.join(p, file)
+    if (fs.statSync(filePath).isDirectory()) {
+      return findTodoInPath(filePath)
+    } else if (file.endsWith(".md")) {
+      return findTodoInFile(filePath)
+    } else {
+      return []
+    }
+  }).reduce((array, current) => array.concat(current), [])
+}
+
+const listTodo = (options) => {
+  const path = options[SUBCOMMAND_PATH] ? options[SUBCOMMAND_PATH].value : "."
+  const todos = findTodoInPath(path);
+  console.log(todos.map((todo) => `${todo.text}: ${todo.file}`).join("\n"));
+}
+
+const todo = (command) => {
+  if (command[COMMAND_LIST_TODO]) {
+    listTodo(command[COMMAND_LIST_TODO])
+  } else {
+    throw Error("Unknown todo command")
+  }
+}
+
+module.exports = {
+  command: todo,
+  options: todoOptions
+}
