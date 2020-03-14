a703bbd77c34c019f01614e4b92a88a761ae9fd5
Charles Feval <chfeval@microsoft.com>
2020-03-14 13:07:44 -0700

    Added link checking

--- src/commands/check.js
+++ src/commands/check.js
@@ -58,20 +58,72 @@ const listTodo = (options) => {
   console.log(todos.map((todo) => `${todo.text}: ${todo.file}`).join("\n"));
 }
 
-const checkLinks = (options) => {
-  const path = options[SUBCOMMAND_PATH] ? options[SUBCOMMAND_PATH].value : "."
-  const linksRegex = RegExp('\\[([^\\]]*)\\]\\(([^)]*)\\)', "g");
-  const files = mapDir(path, ".", (file, dir, filePath) => {
-    const content = fs.readFileSync(filePath).toString();
-    const links = content.match(linksRegex);
+function getAllFiles(options) {
+  const path = options[SUBCOMMAND_PATH] ? options[SUBCOMMAND_PATH].value : ".";
+  const files = mapDir(path, "/", (file, relativePath, filePath) => {
     return {
       file,
-      dir,
-      links,
+      relativePath,
       filePath
-    }
+    };
   });
-  console.log(files);
+  return files;
+}
+
+function filterMdFilesWithInternalLinks(files) {
+  const linksRegex = RegExp("\\[([^\\]]*)\\]\\(([^)]*)\\)", "g");
+  return files.filter((file) => file.file.endsWith(".md"))
+    .map((file) => {
+      const content = fs.readFileSync(file.filePath).toString();
+      const links = [...content.matchAll(linksRegex)].map(entry => ({
+        name: entry[1],
+        link: entry[2]
+      }))
+        .filter(link => !link.link.startsWith("http") &&
+          !link.link.startsWith("/tags/"))
+      file.links = links;
+      return file;
+    })
+    .filter(file => file.links.length > 0);
+}
+
+const displayProblem = (message, file, link) => {
+  console.log(`${message}. File: ${file.relativePath}. Link name: ${link.name}. Link: ${link.link}`)
+}
+
+
+function resolveLink(files, file, link) {
+  const linkPathRegex = new RegExp("(\\w+)(?:#([\\w\\d]*))$")
+  let linkFilePath = link.link.replace(linkPathRegex, "$1");
+  if (!linkFilePath.startsWith("/")) {
+    if (linkFilePath.startsWith("./")) {
+      linkFilePath = linkFilePath.substring(2, linkFilePath.length);
+    }
+    const filePathIndex = file.relativePath.lastIndexOf("/") + 1;
+    const filePath = file.relativePath.substring(0, filePathIndex);
+    linkFilePath = filePath + linkFilePath;
+  }
+  let find = files.find(file => file.relativePath === linkFilePath);
+  return find
+}
+
+const checkLinks = (options) => {
+  const files = getAllFiles(options);
+  const mdFilesWithLinks = filterMdFilesWithInternalLinks(files);
+
+  mdFilesWithLinks.map(mdFile => {
+    mdFile.links
+      .map(link => {
+        if (link.link.indexOf("..") >= 0) {
+          displayProblem(`WARNING: link contains ".." - avoid backwards relative path linking`, mdFile, link)
+        }
+
+        const correspondingFileIndex = resolveLink(files, mdFile, link);
+        if (!correspondingFileIndex) {
+          displayProblem(`ERROR: file not found`, mdFile, link)
+        }
+      })
+  })
 }
 
 const check = (command) => {
